f51681c8a75252848d69b9dff612df0b595714c6
symarys <symarys@gmail.com>
2015-10-12 13:45:36 +0200

    Account information email setting

--- Gemfile
+++ Gemfile
@@ -4,5 +4,4 @@ group :test do
   gem 'byebug'
 end
 
-gem 'clockwork', '~> 1.2.0'
 gem 'sidekiq'
--- init.rb
+++ init.rb
@@ -5,12 +5,14 @@ Redmine::Plugin.register :redmine_new_user_activation do
   version '0.0.1'
   url 'https://github.com/efigence/redmine_new_user_activation'
   author_url 'https://github.com/efigence'
-end
 
-ActionDispatch::Callbacks.to_prepare do
-  require 'redmine_new_user_activation/hooks/activation_date'
-  require 'redmine_new_user_activation/patches/mailer_patch'
-  require 'redmine_new_user_activation/patches/principal_patch'
-  require 'redmine_new_user_activation/patches/users_helper_patch'
-  require 'redmine_new_user_activation/patches/user_patch'
+  settings :default => {'account_information' => ""}, :partial => 'settings/account_information_settings'
+
+  ActionDispatch::Callbacks.to_prepare do
+    require 'redmine_new_user_activation/hooks/activation_date'
+    require 'redmine_new_user_activation/patches/mailer_patch'
+    require 'redmine_new_user_activation/patches/principal_patch'
+    require 'redmine_new_user_activation/patches/users_helper_patch'
+    require 'redmine_new_user_activation/patches/user_patch'
+  end
 end
