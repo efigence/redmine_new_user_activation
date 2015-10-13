486f1fbcb645249801b2a38e0f848d6a143b27e5
symarys <symarys@gmail.com>
2015-10-13 09:31:49 +0200

    Tests for mailer

--- test/unit/mailer_test.rb
+++ test/unit/mailer_test.rb
@@ -5,25 +5,47 @@ class MailerTest < ActionMailer::TestCase
   def setup
     @user = User.new(:firstname => "new", :lastname => "one", :mail => "newone@somenet.foo")
     @user.login ="new_one"
+    Setting.plugin_redmine_new_user_activation['account_information'] = "This is additional text for testing"
   end
 
   def test_should_send_account_information_for_active_user
-    user1 = @user
-    user1.save
+    user = @user
+    user.save
 
-    Mailer.account_information(user1, user1.password).deliver_now
+    Mailer.account_information(user, user.password).deliver_now
 
     assert_emails 1
   end
 
   def test_should_not_send_account_information_for_pending_user
-    user2 = @user
-    user2.activation_date = Date.today+1.month
-    user2.save
+    user = @user
+    user.activation_date = Date.today+1.month
+    user.save
 
-    Mailer.account_information(user2, user2.password).deliver_now
+    Mailer.account_information(user, user.password).deliver_now
 
     assert_emails 0
+  end
+
+  def test_should_sent_redmine_info_for_user_with_company_email
+    user = @user
+    user.mail = "newone@efigence.com"
+    user.save
+
+    Mailer.account_activated(user).deliver_now
+
+    assert_emails 1
+    assert_mail_body_match "This is additional text for testing", ActionMailer::Base.deliveries.last
+  end
+
+  def test_should_not_sent_redmine_info_for_other_users
+    user = @user
+    user.save
+
+    Mailer.account_activated(user).deliver_now
+
+    assert_emails 1
+    assert_mail_body_no_match "This is additional text for testing", ActionMailer::Base.deliveries.last
   end
 
 end
